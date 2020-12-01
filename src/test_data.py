9732e5154c2dfeab61433a8d8666b263594565bd
db5n17 <db5n17@soton.ac.uk>
2020-12-01 21:31:37 +0000

    Added time value and column headings.

--- src/test_data.py
+++ src/test_data.py
@@ -10,13 +10,14 @@ def generate_data(n, file_path, max_voltage, max_temp, max_current):
         os.remove(file_path)
 
     file = open(file_path, "a")
+    file.write("Time,Voltage,Temperature,Current\n")
 
     for i in range(n):
         perc = (1000 - abs((i % 2000) - 1000)) / 1000
         voltage, temp, current = perc * max_voltage, perc * max_temp, perc * max_current
         v_noise, t_noise, c_noise = gauss(0, 5), gauss(0, 2), gauss(0, 0.5)
 
-        line = ",".join(str(val) for val in [max(random(), voltage + v_noise), max(random(), temp + t_noise), max(random(), current + c_noise)])
+        line = ",".join(str(val) for val in [i, max(random(), voltage + v_noise), max(random(), temp + t_noise), max(random(), current + c_noise)])
 
         file.write(line + '\n')
 
