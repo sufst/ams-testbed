c5b90e178877665262196a7db50ad1ef68133cad
db5n17 <db5n17@soton.ac.uk>
2020-12-01 20:04:30 +0000

    Added a general triangle trend line as a placeholder.

--- src/test_data.py
+++ src/test_data.py
@@ -1,4 +1,5 @@
-from random import uniform
+from random import gauss
+from random import random
 import os
 
 def generate_data(n, file_path, max_voltage, max_temp, max_current):
@@ -9,14 +10,13 @@ def generate_data(n, file_path, max_voltage, max_temp, max_current):
         os.remove(file_path)
 
     file = open(file_path, "a")
-    voltage = 0.98 * max_voltage
-    temp = 0.65 * max_temp
-    current = (voltage / max_voltage) * max_current
 
     for i in range(n):
-        v_noise, t_noise, c_noise = uniform(-1, 1), uniform(-1, 1), uniform(-1, 1)
+        perc = (1000 - abs((i % 2000) - 1000)) / 1000
+        voltage, temp, current = perc * max_voltage, perc * max_temp, perc * max_current
+        v_noise, t_noise, c_noise = gauss(0, 5), gauss(0, 2), gauss(0, 0.5)
 
-        line = ",".join(str(val) for val in [voltage + v_noise, temp + t_noise, current + c_noise])
+        line = ",".join(str(val) for val in [max(random(), voltage + v_noise), max(random(), temp + t_noise), max(random(), current + c_noise)])
 
         file.write(line + '\n')
 
